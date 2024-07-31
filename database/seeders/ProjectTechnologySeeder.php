e1f53abe8b5b48832293fab33b50813f45dbc42e
LorezoParis1996 <lorenzoparis86@live.it>
2024-07-31 15:24:54 +0200

    seeding di ProjectTechnologySeeder fatto

--- database/seeders/ProjectTechnologySeeder.php
+++ database/seeders/ProjectTechnologySeeder.php
@@ -4,14 +4,22 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Technology;
+use App\Models\Project;
+use Faker\Generator as Faker;
 
 class ProjectTechnologySeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        $projects = Project::all();
+        $technologies = Technology::all()->pluck('id');
+
+        foreach($projects as $project) {
+            $project->technologies()->attach($faker->randomElements($technologies, rand(2, 5)));
+        }
     }
 }
